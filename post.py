ea96632b6072e567fdd9864068c29ac612b88376
kenvifire <mrwhite@163.com>
2013-11-05 20:25:53 +0800

    post py

--- post.py
+++ post.py
@@ -9,7 +9,7 @@ except MySQLdb.ERROR,e:
    print "Error %d:%s"%(e.args[0],e.args[1])
    exit(1)
 cursor = db.cursor()
-cursor.execute('select * from wp_post')
+cursor.execute('select * from wp_posts')
 result_set=cursor.fetchall()
 print result_set
 cursor.close()

ab406026f3a9679483657cdc5c5bbfbfa00c453b
kenvifire <mrwhite@163.com>
2013-11-05 20:12:37 +0800

    add post

---
+++ post.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+import os,sys
+import  MySQLdb
+
+try:
+   db = MySQLdb.connect(host ='127.0.0.1', user='wordpress', passwd='wordpress6826', db ='wordpress')
+except MySQLdb.ERROR,e:
+   print "Error %d:%s"%(e.args[0],e.args[1])
+   exit(1)
+cursor = db.cursor()
+cursor.execute('select * from xxx_task')
+result_set=cursor.fetchall()
+print result_set
+cursor.close()
+db.close()
